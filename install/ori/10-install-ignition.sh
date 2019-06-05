f31e0d00fb2b21bff9540d176845e0aa109be8ff
Christophe Lehy <christophe.lehy@hpe.com>
2019-06-05 12:01:18 +0000

    tuning for doc

--- install/ori/10-install-ignition.sh
+++ install/ori/10-install-ignition.sh
@@ -8,4 +8,4 @@ cp ./install-config.yaml $INSDIR
 base64 -w0 $INSDIR/master.ign >$INSDIR/master.64
 base64 -w0 $INSDIR/worker.ign >$INSDIR/worker.64
 base64 -w0 $INSDIR/append-bootstrap.ign >$INSDIR/append-bootstrap.64
-scp $INSDIR/bootstrap.ign clh-infra:/var/www/html
+scp $INSDIR/bootstrap.ign localhost:/var/www/html
